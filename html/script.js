bbdf198da60c2682f203d3b48b53316c486fb23e
Eric A. Meyer <eric@meyerweb.com>
2022-09-13 17:37:17 -0400

    Improved JS, CSS

--- html/script.js
+++ html/script.js
@@ -1,14 +1,29 @@
 window.onload = function() {
 	if (window.history) {
+
+		let fnoteLinks = document.querySelectorAll('a[href^="#fnote"]');
 		let closeLinks = document.querySelectorAll('a.close');
+
+		for (let fnLink of fnoteLinks) {
+			fnLink.addEventListener('click',fncloser);
+		}
 		for (let cLink of closeLinks) {
 			cLink.addEventListener('click',fncloser);
 		}
-	}
-}
+		window.addEventListener('keyup',keyClose);
 
-function fncloser() {
-	let caller = '#' + this.parentElement.getAttribute('id');
-	let fragID = window.location.hash;
-	if (caller == fragID) window.history.back();
+		function fncloser() {
+			let fragID = window.location.hash;
+			if (fragID == '') return;
+			let caller = '#' + this.parentElement.getAttribute('id');
+			if (caller == '#null') caller = this.getAttribute('href');
+			if (caller == fragID) window.history.back();
+		}
+		
+		function keyClose(e) {
+			if (window.location.hash.search('fnote') < 0) return;
+			if (e.key == "Escape") window.history.back();
+		}
+
+	}
 }
